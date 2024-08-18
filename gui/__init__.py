9f3473f49b1940712d3678350918c098e0c2d918
PAC Lab <rodgers.paclab@gmail.com>
2024-08-18 14:36:28 -0400

    make gui a module and doc its files

---
+++ gui/__init__.py
@@ -0,0 +1,16 @@
+"""The gui module provides scripts to run the desktop GUI.
+
+start_gui.py
+    This is the scripts that is run by the user to start the GUI. It collects
+    the box name from sys.argv and instantiates main_window.MainWindow.
+
+main_window.py
+    This defines the MainWindow class which is the main window of the GUI.
+
+config_dialog.py
+
+logging.py
+
+plotting.py
+    
+"""
