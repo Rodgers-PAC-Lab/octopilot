be1286c74739e95bc172358f8e5bdd346589fe7d
Sukrith Sriram Vedapuram <svedapuram3@gatech.edu>
2024-08-22 12:03:32 -0400

    changing module code

--- blink/blink_test.py
+++ blink/blink_test.py
@@ -14,18 +14,16 @@ class BlinkTest:
         self.blink_socket = self.blink_context.socket(zmq.PUB)
         self.blink_socket.bind(address)
 
-    def send_message(self):
+    def send_message(self, msg=None):
         if self.blink_state == True:
-            self.blink_socket.send_string("blink")
+            msg = "blink"
+            self.blink_socket.send_string(msg)
             print(f"Sent 'blink' to Pis")
 
         elif self.blink_state == False or self.blink_state == None:
-            self.blink_socket.send_string("waiting")
+            msg = "waiting"
+            self.blink_socket.send_string(msg)
             pass
-
-    # def set_blink_state(self, state):
-    #     self.blink_state = state
-    #     print(f"Blink state set to {self.blink_state}")
 
 # Entry point for running the module as a script
 if __name__ == "__main__":
