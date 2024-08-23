57ff587fcdde2fcf8f7ec47b67f7f3098f7d1189
Sukrith Sriram Vedapuram <svedapuram3@gatech.edu>
2024-08-23 13:11:17 -0400

    debug

--- pi_test.py
+++ pi_test.py
@@ -941,10 +941,10 @@ try:
                 amplitude_max = config_data['amplitude_max']
 
             # Setting sound to play 
-            # sound_chooser.update_parameters(
-            #     rate_min, rate_max, irregularity_min, irregularity_max, 
-            #     amplitude_min, amplitude_max, center_freq_min, center_freq_max, bandwidth)
-            sound_chooser.set_sound_cycle()
+            sound_chooser.update_parameters(
+                rate_min, rate_max, irregularity_min, irregularity_max, 
+                amplitude_min, amplitude_max, center_freq_min, center_freq_max, bandwidth)
+            #sound_chooser.set_sound_cycle()
 
         # Separate logic for Poketrain task
         if task == 'Poketrain':
