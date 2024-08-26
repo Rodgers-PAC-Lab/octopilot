182472dd87e13faf478fcff452f4809656674fa1
Sukrith Sriram Vedapuram <svedapuram3@gatech.edu>
2024-08-26 13:56:02 -0400

    fixing loud noise on trial complete

--- pi_test.py
+++ pi_test.py
@@ -1138,6 +1138,8 @@ try:
                 new_params = sound_chooser.update_parameters(
                     rate_min, rate_max, irregularity_min, irregularity_max, 
                     amplitude_min, amplitude_max, center_freq_min, center_freq_max, bandwidth)
+                sound_chooser.initialize_sounds(sound_player.blocksize, sound_player.fs, 
+                    sound_chooser.amplitude, sound_chooser.target_highpass, sound_chooser.target_lowpass)
                 poke_socket.send_string(new_params)
                 
                 # Turn off the currently active LED
