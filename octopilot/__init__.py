cd0a3ba7437c268da8350872804b072e2d7580b9
Chris Rodgers <xrodgers@gmail.com>
2024-09-15 15:32:16 -0400

    doc module

---
+++ octopilot/__init__.py
@@ -0,0 +1,7 @@
+"""Main octopilot module.
+
+See ../README for documentation.
+
+This will not import anything, because generally we want to import 
+octopilot.pi or octopilot.desktop, but not both.
+"""
