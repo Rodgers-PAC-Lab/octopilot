61c7126f983b2cc8dade2e443e870e9c2a80427b
Chris Rodgers <xrodgers@gmail.com>
2024-09-15 15:28:51 -0400

    match setup.py setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='octopilot',
+    version='0.1',
+    description='Running behavior with octopilot',
+    author='Rodgers PAC Lab',
+    author_email='rodgers.paclab@gmail.com',
+    packages=['octopilot'],
+    install_requires=[],
+)
