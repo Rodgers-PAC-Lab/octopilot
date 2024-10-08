fc4b31a9beb8dd30c966fcf6edf4e33f9f2a0f78
PAC Lab <rodgers.paclab@gmail.com>
2024-10-08 15:03:17 -0400

    debug

--- pi.py
+++ pi.py
@@ -1126,7 +1126,7 @@ try:
                 flash()
                 
                 # Making previous reward port none to avoid blocking
-                prev_reward = None
+                #prev_reward = None
                 
                 #open_valve(prev_port)
                 
