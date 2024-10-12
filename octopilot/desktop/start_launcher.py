12dc582cbc80c4e616b1a737085b6d817e62c06b
Chris Rodgers <xrodgers@gmail.com>
2024-10-12 19:02:36 -0400

    launcher start

--- octopilot/desktop/start_launcher.py
+++ octopilot/desktop/start_launcher.py
@@ -17,17 +17,62 @@ import threading
 import argparse
 import sys
 import signal
+import subprocess
 import glob
 import os
 import pandas
 
+# Helper function
+def start_octopilot_gui_in_new_terminal(
+    mouse, box, task, zoom=0.6, ncols=80, nrows=30, 
+    xpos=100, ypos=200, keep_window_open=True):
+    """Open a terminal and start an octopilot GUI session in it.
+    
+    We want to run the octopilot GUI session in its own subprocess so that 
+    the Launcher will keep running even if that session crashes. We also
+    want to be able to see the debug messages in the terminal. So we
+    open a subprocess that runs a bash command to start the Octopilot session.
+    
+    Arguments
+    ---------
+    mouse, box, task : str
+        These are passed as argparse kwargs to octopilot start_gui like this:
+        python3 -m octopilot.desktop.start_gui {mouse} {box} {task}
+    
+    zoom, ncols, nrows, xpos, ypos : numeric
+        These are used to set up how the gnome-terminal window appears
+    
+    Returns: the result of subprocess.Popen
+    """
+    # This is the python3 command used to start the octopilot session
+    bash_command = (
+        f'python3 -m octopilot.desktop.start_gui {mouse} {box} {task}')
 
-def call_external(mouse, box, task, **other_python_parameters):
-    print(mouse, box, task)
-    #~ ArduFSM.Runner.start_runner_cli.main(mouse=mouse, board=board, box=box,
-        #~ experimenter=experimenter,
-        #~ **other_python_parameters)
+    # Optionally add a 'read' command afterward to keep the window open
+    if keep_window_open:
+        bash_command += '; read'    
 
+    # Form the full list of Popen args, which are used to start the
+    # gnome-terminal window and then run the bash_command inside it
+    popen_args = [
+        'gnome-terminal', 
+        '--geometry=%dx%d+%d+%d' % (ncols, nrows, xpos, ypos),
+        '--zoom=%0.2f' % zoom,  
+        '--', # used to be -x
+        'bash', '-l', '-c',
+        ]
+    
+    # Enclose bash_command in quotes and append to popen_args
+    popen_args.append('"' + bash_command + '"')
+    
+    # Print
+    print(' '.join(popen_args))
+    
+    # Popen
+    # TODO: keep track of this process in Launcher
+    proc = subprocess.Popen(popen_args)
+    
+    return proc
 
 ## Use argparse to identify the box, mouse, and task
 parser = argparse.ArgumentParser(
@@ -69,9 +114,13 @@ print(mouse_records)
 ## Create the main window
 class LauncherWindow(QWidget):
     def __init__(self, mouse_records):
+        # Super init for QWidget
         super().__init__()
+        
+        # Set window title
         self.setWindowTitle('Octopilot Launcher')
         
+        # Create a table
         self.table_widget = QTableWidget()
         self.table_widget.setRowCount(1 + len(mouse_records))
         self.table_widget.setColumnCount(4)
@@ -80,7 +129,7 @@ class LauncherWindow(QWidget):
         self.table_widget.setItem(0, 2, QTableWidgetItem('Task'))
         self.table_widget.setItem(0, 3, QTableWidgetItem('Start'))
 
-        # Put each
+        # Put data for each mouse in a row
         for n_mouse, mouse in enumerate(mouse_records.index):
             n_row = n_mouse + 1
             
@@ -100,35 +149,67 @@ class LauncherWindow(QWidget):
             self.table_widget.setItem(n_row, 2, item )
             
             # The start button
+            # Connect this to start_session_from_qb, using `qb` as an argument
             qb = QPushButton('Start')
-            qb.clicked.connect(functools.partial(self.start_session2, qb))
+            qb.clicked.connect(
+                functools.partial(self.start_session_from_qb, qb))
             self.table_widget.setCellWidget(n_row, 3, qb)
 
+        # Create a layout
         self.layout = QVBoxLayout()
+        
+        # Add table to the layout
         self.layout.addWidget(self.table_widget)
+        
+        # Set the layout
         self.setLayout(self.layout)
 
-    def start_session(self, row):
-        """Collect data from row and pass to start session"""
+    def start_session_from_row_idx(self, n_row):
+        """Use data from row in self.table_widget to start octopilot session
+        
+        n_row : int
+            The index of the row within self.table_widget corresponding
+            to the session to start
+        """
         # Highlight clicked cell
-        self.table_widget.setCurrentCell(row, 3)
+        self.table_widget.setCurrentCell(n_row, 3)
         
         # Extract data from row
-        mouse = str(self.table_widget.item(row, 0).text())
-        box = str(self.table_widget.item(row, 1).text())
-        task = str(self.table_widget.item(row, 2).text())
+        mouse = str(self.table_widget.item(n_row, 0).text())
+        box = str(self.table_widget.item(n_row, 1).text())
+        task = str(self.table_widget.item(n_row, 2).text())
+
+        # Use the box name to get the position
+        if box == 'box2':
+            ypos = 100
+        elif box == 'box3':
+            ypos = 500
+        elif box == 'box4':
+            ypos = 900
+        elif box == 'box5':
+            ypos = 1300
         
-        # Call
-        call_external(
+        # xpos is fixed
+        xpos = 300
+        
+        # Call start_octopilot_gui_in_new_terminal with that data
+        # TODO: keep track of this process
+        proc = start_octopilot_gui_in_new_terminal(
             mouse=mouse,
             box=box,
             task=task,
-            background_color=None,
+            xpos=xpos,
+            ypos=ypos,
         )
 
-    def start_session2(self, row_qb):
+    def start_session_from_qb(self, row_qb):
         """Start the session associated with the push button for this row.
         
+        This is connected to pushbutton click. The reason to pass the
+        pushbutton itself, instead of the row index, is in case the 
+        rows get rearranged (which is not currently possible, but should be).
+        
+        row_qb : QPushButton that was clicked
         """
         # Find which row the push button is in
         session_row = -1
@@ -140,7 +221,8 @@ class LauncherWindow(QWidget):
             raise ValueError("cannot find row for pushbutton")
         
         # Extract the mouse, board, box from this row
-        self.start_session(session_row)
+        self.start_session_from_row_idx(session_row)
+
 ## Start
 if __name__ == '__main__':
     # Apparently QApplication needs sys.argv for some reason
