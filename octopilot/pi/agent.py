d0a819ac9053042808c3723353fbd38412a8f7c1
PAC Lab <rodgers.paclab@gmail.com>
2024-10-16 18:45:38 -0400

    raise autopoke to 10

--- octopilot/pi/agent.py
+++ octopilot/pi/agent.py
@@ -158,8 +158,8 @@ class Agent(object):
         # Autopoke
         # This simulates the presence of a mouse, which may be poking before
         # the session actually starts
-        self.left_nosepoke.autopoke_start(rate=1)
-        self.right_nosepoke.autopoke_start(rate=1)
+        self.left_nosepoke.autopoke_start(rate=10)
+        self.right_nosepoke.autopoke_start(rate=10)
 
         
         ## Optionally set up networking
