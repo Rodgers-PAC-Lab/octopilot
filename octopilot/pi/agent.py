f070a1625a851c5f1d217e69c111b74f23d8f135
PAC Lab <rodgers.paclab@gmail.com>
2024-11-12 14:59:45 -0500

    fix

--- octopilot/pi/agent.py
+++ octopilot/pi/agent.py
@@ -262,8 +262,8 @@ class Agent(object):
 
         # Log the time of the flash
         # Do this after the flash itself so that we don't jitter
-        self.left_nose_poke.flash_red_led()
-        self.right_nose_poke.flash_red_led()
+        self.left_nosepoke.flash_red_led()
+        self.right_nosepoke.flash_red_led()
         self.logger.debug(
             "[{}] synchronization_flash".format(timestamp)
             )        
