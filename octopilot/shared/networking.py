3bc1d396740c0e228ffd441dafbea7a2c7da63f9
PAC Lab <rodgers.paclab@gmail.com>
2024-12-11 14:21:40 -0500

    fixing check bonsai socket

--- octopilot/shared/networking.py
+++ octopilot/shared/networking.py
@@ -605,14 +605,14 @@ class PiNetworkCommunicator(object):
                     self.logger.debug(
                         f'{dt_now} - Received message {self.bonsai_state} on bonsai socket')
                     self.prev_bonsai_state = self.bonsai_state
-            elif self.bonsai_state == 'False':
+            if self.bonsai_state == 'False':
                 if self.prev_bonsai_state == 'True':
                     self.logger.debug(
                             f'{dt_now} - Received message {self.bonsai_state} on bonsai socket')
                     self.prev_bonsai_state = self.bonsai_state
                 else:
                     self.prev_bonsai_state = self.bonsai_state
-            elif self.bonsai_state == 'True':
+            if self.bonsai_state == 'True':
                 if self.prev_bonsai_state == 'False':
                     self.logger.debug(
                             f'{dt_now} - Received message {self.bonsai_state} on bonsai socket')
