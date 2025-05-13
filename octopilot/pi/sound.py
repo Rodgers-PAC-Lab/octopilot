e9493a0b50e3bc413a9ce4a3fd115537632454ec
PAC Lab <rodgers.paclab@gmail.com>
2025-05-13 12:50:26 -0400

    Revert "debug"
    This reverts commit 12c2929800462dc92b041e437ab865e73bc8300e.

--- octopilot/pi/sound.py
+++ octopilot/pi/sound.py
@@ -492,8 +492,6 @@ class SoundGenerator_IntermittentBursts(object):
                 # Append the gap between sounds
                 append_gap(bdrow.gap_chunks)        
 
-        print(self.one_cycle_of_audio_frames)
-
     def set_audio_parameters(self, left_params, right_params):
         """Define self.sound_cycle, to go through sounds
         
