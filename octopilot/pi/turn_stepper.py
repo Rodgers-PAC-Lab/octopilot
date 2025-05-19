3bc4eab169f7714422bf3630760e75d5e88305b3
PAC Lab <rodgers.paclab@gmail.com>
2025-05-19 17:36:56 -0400

    update

--- octopilot/pi/turn_stepper.py
+++ octopilot/pi/turn_stepper.py
@@ -11,7 +11,7 @@ import numpy as np
 import os
 import itertools
 import importlib
-from . import hardware
+#from . import hardware
 
 ## Killing previous pigpiod and jackd background processes
 os.system('sudo killall pigpiod')
@@ -39,13 +39,30 @@ time.sleep(1)
 
 
 ## Keep track of pigpio.pi
-pi = pigpio.pi()
+pig = pigpio.pi()
 
 
 ## Run
+# 200 steps/rev
+# At 256x, this is 51200 steps/rev
+# To do 1 rps, ISI is 1/51200 s, likely unachievable
+# about 0.3 ms of overhead, so fastest is 3000 steps/s
+# For some reason the overhead increases over time
+pulse_time = 1e-8 # min
+isi = 1/1000
+corrected_isi = isi - 0.0004
+if corrected_isi < 1e-8:
+    corrected_isi = 1e-8
+print(corrected_isi)
+now = datetime.datetime.now()
+n_steps = 0
 while True:
     pig.write(26, 1)
-    time.sleep(.01)
+    time.sleep(pulse_time)
     pig.write(26, 0)
-    time.sleep(.01)
+    time.sleep(corrected_isi)
+    n_steps += 1
+    if np.mod(n_steps, 100) == 0:
+        time_taken = (datetime.datetime.now() - now).total_seconds()
+        print(f'{n_steps} in {time_taken} = {n_steps / time_taken}')
 
