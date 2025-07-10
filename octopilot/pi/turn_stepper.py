80c89c0bf58f020f19085238da5057c15a1c3cef
PAC Lab <rodgers.paclab@gmail.com>
2025-07-10 17:33:59 -0400

    impement gpio in turn_stpper

--- octopilot/pi/turn_stepper.py
+++ octopilot/pi/turn_stepper.py
@@ -46,23 +46,53 @@ pig = pigpio.pi()
 # 200 steps/rev
 # At 256x, this is 51200 steps/rev
 # To do 1 rps, ISI is 1/51200 s, likely unachievable
-# about 0.3 ms of overhead, so fastest is 3000 steps/s
+# about 0.4 ms of overhead, so fastest is 3000 steps/s
 # For some reason the overhead increases over time
-pulse_time = 1e-8 # min
-isi = 1/1000
-corrected_isi = isi - 0.0004
+# With RPi.GPIO, the overhead is slightly less, about 0.15 ms of overhead
+meth = 'pigpio'
+if meth == 'pigpio':
+    correction_factor = 0.0004
+elif meth == 'rpi':
+    correction_factor = 0.00015
+else:
+    1/0
+
+# How long to make the pulse, essentially the minimum possible
+pulse_time = 1e-6 # min
+
+# Use the isi to set the period
+desired_frequency = 3000
+isi = 1 / desired_frequency
+
+# Apply the correction factor to the ISI
+corrected_isi = isi - correction_factor
+
+# Floor the ISI
 if corrected_isi < 1e-8:
     corrected_isi = 1e-8
-print(corrected_isi)
+
+print(f'will wait {corrected_isi} s')
+
+# Run
 now = datetime.datetime.now()
 n_steps = 0
 while True:
-    pig.write(26, 1)
-    time.sleep(pulse_time)
-    pig.write(26, 0)
-    time.sleep(corrected_isi)
+    
+    if meth == 'pigpio':
+        pig.write(26, 1)
+        time.sleep(pulse_time)
+        pig.write(26, 0)
+        time.sleep(corrected_isi)
+    elif meth == 'rpi':
+        GPIO.output(26, 1)
+        time.sleep(pulse_time)
+        GPIO.output(26, 0)
+        time.sleep(corrected_isi)
+    else:
+        1/0
+    
     n_steps += 1
-    if np.mod(n_steps, 100) == 0:
+    if np.mod(n_steps, desired_frequency) == 0:
         time_taken = (datetime.datetime.now() - now).total_seconds()
         print(f'{n_steps} in {time_taken} = {n_steps / time_taken}')
 
